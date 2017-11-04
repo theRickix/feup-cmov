1d09516ed17dca1f27775c446ebb2d23e1fa60fb
killghost15 <killghost15@gmail.com>
2017-11-04 02:14:22 +0000

    routes n sei como se faz no ApiService para se ligar as funções

--- first-project/client-app/Shop&Pay/app/src/main/java/api/ApiService.java
+++ first-project/client-app/Shop&Pay/app/src/main/java/api/ApiService.java
@@ -1,6 +1,7 @@
 package api;
 
 import data.ProductList;
+import data.User;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -8,4 +9,5 @@ public interface ApiService {
 
     @GET("products")
     Call<ProductList> getProducts();
+    Call<User> login();
 }
--- first-project/server/routes/index.js
+++ first-project/server/routes/index.js
@@ -9,6 +9,7 @@ router.get('/api/products/:id', db.getSingleProduct);
 router.post('/api/products', db.createProduct);
 router.put('/api/products/:id', db.updateProduct);
 router.delete('/api/products/:id', db.removeProduct);
-
+router.post('/api/users',db.register);
+router.get('/api/users/:username',db.login);
 
 module.exports = router;
