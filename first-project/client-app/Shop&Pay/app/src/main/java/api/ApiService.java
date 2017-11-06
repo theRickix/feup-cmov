f01737a92d5f1bbfc6afdbf2392a18b36807dba2
Ricardo Magalhães <ricardo_magalhaes45@hotmail.com>
2017-11-06 14:53:04 +0000

    Added more fields to User class

--- first-project/client-app/Shop&Pay/app/src/main/java/api/ApiService.java
+++ first-project/client-app/Shop&Pay/app/src/main/java/api/ApiService.java
@@ -3,11 +3,18 @@ package api;
 import data.ProductList;
 import data.User;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiService {
 
     @GET("products")
     Call<ProductList> getProducts();
+
+    @GET("users")
     Call<User> login();
+
+    @POST("users")
+    Call<User> register(@Body User user);
 }
--- first-project/client-app/Shop&Pay/app/src/main/java/data/User.java
+++ first-project/client-app/Shop&Pay/app/src/main/java/data/User.java
@@ -3,6 +3,8 @@ package data;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.sql.Date;
+
 /**
  * Created by JP on 04/11/2017.
  */
@@ -12,18 +14,49 @@ public class User {
     @Expose
     private int id;
 
+    @SerializedName("name")
+    @Expose
+    private String name;
+
     @SerializedName("username")
     @Expose
     private String username;
+
+    @SerializedName("email")
+    @Expose
+    private String email;
 
     @SerializedName("password")
     @Expose
     private String password;
 
-    public User(int id,String username,String password){
-        this.id=id;
-        this.username=username;
-        this.password=password;
+    @SerializedName("fiscal")
+    @Expose
+    private String fiscal;
+
+    @SerializedName("cc_type")
+    @Expose
+    private CardType cc_type;
+
+    @SerializedName("cc_number")
+    @Expose
+    private String cc_number;
+
+    @SerializedName("cc_date")
+    @Expose
+    private Date cc_date;
+
+    public User(int id, String name, String username, String email,
+                String password, String fiscal, CardType cc_type, String cc_number, Date cc_date) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.fiscal = fiscal;
+        this.cc_type = cc_type;
+        this.cc_number = cc_number;
+        this.cc_date = cc_date;
     }
 
     public int getId() {
@@ -34,6 +67,14 @@ public class User {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getUsername() {
         return username;
     }
@@ -42,11 +83,51 @@ public class User {
         this.username = username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFiscal() {
+        return fiscal;
+    }
+
+    public void setFiscal(String fiscal) {
+        this.fiscal = fiscal;
+    }
+
+    public CardType getCc_type() {
+        return cc_type;
+    }
+
+    public void setCc_type(CardType cc_type) {
+        this.cc_type = cc_type;
+    }
+
+    public String getCc_number() {
+        return cc_number;
+    }
+
+    public void setCc_number(String cc_number) {
+        this.cc_number = cc_number;
+    }
+
+    public Date getCc_date() {
+        return cc_date;
+    }
+
+    public void setCc_date(Date cc_date) {
+        this.cc_date = cc_date;
     }
 }
