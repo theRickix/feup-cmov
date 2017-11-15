3c36e6e69ad7f7f562148cfdca2489e76a81cd46
Ricardo Magalhães <ricardo_magalhaes45@hotmail.com>
2017-11-15 21:14:49 +0000

    Deploying to heroku

--- first-project/server/queries.js
+++ first-project/server/queries.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://shopping:shopping@localhost:5432/shopping';
+var connectionString = process.env.DATABASE_URL;
 var db = pgp(connectionString);
 
 // add query functions
