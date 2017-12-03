cdfd801a55fa5b9f839dff697a9bd99eeac39daa
killghost15 <killghost15@gmail.com>
2017-12-03 22:16:23 +0000

    callpage

---
+++ WeatherApp/WeatherApp/WeatherApp/CallPage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Xamarin.Forms;
+
+namespace XamCallWS {
+  public class CallPage : ContentPage {
+    Label lab1, lab2;
+    Entry entTo, entFrom;
+
+    public CallPage() {
+      Title = "Call Web Service";
+      var labFrom = new Label() {
+        Text = "Convert from:",
+        WidthRequest = 120
+      };
+      entFrom = new Entry() {
+        HorizontalOptions = LayoutOptions.StartAndExpand,
+        Placeholder = "Currency code"
+      };
+      var stack1 = new StackLayout() {
+        Orientation = StackOrientation.Horizontal,
+        Children = { labFrom, entFrom }
+      };
+
+      var labTo = new Label() {
+        Text = "Convert to:",
+        WidthRequest = 120
+      };
+      entTo = new Entry() {
+        HorizontalOptions = LayoutOptions.StartAndExpand,
+        Placeholder = "Currency code"
+      };
+      var stack2 = new StackLayout() {
+        Orientation = StackOrientation.Horizontal,
+        Children = { labTo, entTo }
+      };
+
+      var but = new Button() {
+        Text = "Call",
+        HorizontalOptions = LayoutOptions.Center
+      };
+      but.Clicked += OnButton_Clicked;
+
+      lab1 = new Label() {
+        HorizontalOptions = LayoutOptions.Center,
+        Text = "Status: "
+      };
+
+      lab2 = new Label() {
+        Text = ""
+      };
+
+      Content = new StackLayout {
+        Children = { stack1, stack2, but, lab1, lab2 }
+      };
+    }
+
+    private void OnButton_Clicked(object sender, EventArgs e) {
+      var uri = string.Format("https://api.fixer.io/latest?base={0};symbols={1}", entFrom.Text, entTo.Text);
+      var cb = new AsyncCallback(CallHandler);
+      CallWebAsync(uri, lab1, lab2, cb);
+    }
+
+    private void CallWebAsync(string uri, Label status, Label response, AsyncCallback cb) {
+      var request = HttpWebRequest.Create(uri);
+      request.Method = "GET";
+      var state = new Tuple<Label, Label, WebRequest>(status, response, request);
+
+      request.BeginGetResponse(cb, state);
+    }
+
+    private void CallHandler(IAsyncResult ar) {
+      var state = (Tuple<Label, Label, WebRequest>)ar.AsyncState;
+      var request = state.Item3;
+
+      using (HttpWebResponse response = request.EndGetResponse(ar) as HttpWebResponse) {
+        Device.BeginInvokeOnMainThread(() => state.Item1.Text = "Status: " + response.StatusCode);
+        if (response.StatusCode == HttpStatusCode.OK)
+          using (StreamReader reader = new StreamReader(response.GetResponseStream())) {
+            var content = reader.ReadToEnd();
+            Device.BeginInvokeOnMainThread(() => state.Item2.Text = content);
+          }
+      }
+    }
+
+  }
+}
