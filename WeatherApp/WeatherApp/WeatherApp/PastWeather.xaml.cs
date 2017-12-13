72a5cf31e5569f9a29e3cfe592a3b7b54ba3f802
killghost15 <killghost15@gmail.com>
2017-12-13 03:40:25 +0000

    gráfico

--- WeatherApp/WeatherApp/WeatherApp/PastWeather.xaml.cs
+++ WeatherApp/WeatherApp/WeatherApp/PastWeather.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Microcharts;
+using SkiaSharp;
+using WeatherApp.ViewModels;
 
 namespace WeatherApp
 {
@@ -12,6 +15,24 @@ namespace WeatherApp
         public PastWeather()
         {
             InitializeComponent();
+            var entries = new Microcharts.Entry[8];
+
+            var vm = (PastWeatherViewModel)this.BindingContext;
+            List<Models.Hour> hours = vm.PastWeatherModel.NeededHours;
+            int i = 0;
+            foreach (Models.Hour hour in hours) {
+                
+                entries[i] = new Microcharts.Entry((float)hour.temp_c)
+                {
+                    Label = hour.time,
+                    ValueLabel= hour.temp_c.ToString(),
+                    Color=SKColor.Parse("#266489")
+
+                };
+                    i++;
+            }
+            var chart = new LineChart() { Entries = entries };
+            this.chartView.Chart = chart;
         }
     }
 }
